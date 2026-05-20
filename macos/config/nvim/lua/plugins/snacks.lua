f20e48bf2ceece0c0e71f6e7e688f5a827664fb3
toni <tmunozferrer@gmail.com>
2026-05-20 19:07:10 +0200

    modified:   macos/config/nvim/lua/plugins/snacks.lua

--- macos/config/nvim/lua/plugins/snacks.lua
+++ macos/config/nvim/lua/plugins/snacks.lua
@@ -5,22 +5,8 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        dashboard = {
-            enabled = true,
-            preset = {
-                keys = {
-                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                    { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                    { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-                    { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                },
-            },
-        },
+        dashboard = { enabled = true },
         explorer = { enabled = true },
-        image = { enabled = true },
         indent = { enabled = true },
         lazygit = { enabled = true },
         notifier = { enabled = true },
