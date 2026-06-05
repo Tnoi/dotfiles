8dd5270f261f4b14bef5fb3fe03a42eaa961c3e4
toni <tmunozferrer@gmail.com>
2026-06-05 15:05:19 +0200

    modified: init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -52,10 +52,27 @@ vim.keymap.set('n', '<leader>fb', function() require('fzf-lua').buffers() end, {
 vim.keymap.set('n', '<leader>fg', function() require('fzf-lua').live_grep() end, { desc = "Grep" })
 vim.keymap.set('n', '<leader>fr', function() require('fzf-lua').history() end, { desc = "History" })
 
+vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', { desc = "Explorer" })
+
+vim.keymap.set("n", "<leader>gp", function() require('mini.diff').toggle_overlay() end, { desc = "Preview diff overlay" })
+
 vim.keymap.set('n', '<leader>y', function() require('yazi').yazi() end, { desc = "Yazi" })
 vim.keymap.set('n', '<leader>gg', function() require('lazygit').lazygit() end, { desc = "LazyGit" })
 
 vim.keymap.set('n', '<leader>u', '<cmd>Undotree<cr>', { desc = "Undotree" })
+
+vim.keymap.set('n', '<leader>bn', '<cmd>enew<cr>', { desc = 'New buffer' })
+vim.keymap.set("n", "<leader>bb", "<C-^>", { desc = "Last buffer" })
+vim.keymap.set('n', '<leader>bd', function() require('mini.bufremove').delete() end, { desc = 'Delete buffer' })
+
+vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<cr>", { desc = "Split right" })
+vim.keymap.set("n", "<leader>wh", "<cmd>split<cr>", { desc = "Split down" })
+vim.keymap.set("n", "<leader>wd", "<cmd>close<cr>", { desc = "Close window" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 
 vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result (centered)' })
 vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result (centered)' })
@@ -75,8 +92,7 @@ vim.keymap.set('v', "<", "<gv", { desc = 'Indent left and reselect' })
 vim.keymap.set('v', "<Tab>", ">gv", { desc = 'Indent right and reselect' })
 vim.keymap.set('v', "<S-Tab>", "<gv", { desc = 'Indent left and reselect' })
 
-vim.keymap.set('n', '<leader>n', '<cmd>enew<cr>', { desc = 'New' })
-vim.keymap.set('n', '<D-s>', '<cmd>w<cr>', { desc = 'Save' })
+vim.keymap.set({ 'n', 'i' }, '<D-s>', '<Esc><cmd>w<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>qq', '<cmd>q<cr>', { desc = 'Quit' })
 vim.keymap.set('n', '<leader>q!', '<cmd>q!<cr>', { desc = 'Force quit' })
 
@@ -94,6 +110,11 @@ vim.pack.add({
 
     'https://github.com/ibhagwan/fzf-lua',
 
+    'https://github.com/nhat-vo/neo-tree.nvim',
+    'https://github.com/MunifTanjim/nui.nvim',
+
+    'https://github.com/nvim-mini/mini.nvim',
+
     'https://github.com/folke/which-key.nvim',
 
     'https://github.com/mikavilpas/yazi.nvim',
@@ -110,7 +131,77 @@ require('lualine').setup({ options = { theme = 'gruvbox' } })
 
 require("fzf-lua").setup()
 
-require('which-key').setup({ preset = "modern" })
+require("neo-tree").setup({
+    filesystem = {
+        use_libuv_file_watcher = true,
+        filtered_items = {
+            visible = true,
+            never_show = {
+                '.git',
+            },
+        },
+    },
+    window = {
+        width = 35
+    },
+    default_component_configs = {
+        icon = {
+            folder_empty = "󰜌",
+            folder_empty_open = "󰜌",
+        },
+        git_status = {
+            symbols = {
+                added     = "",
+                modified  = "",
+                deleted   = "✖",
+                renamed   = "󰁕",
+
+                untracked = "",
+                ignored   = "",
+                unstaged  = "󰄱",
+                staged    = "",
+                conflict  = "",
+            },
+        },
+    },
+    document_symbols = {
+        kinds = {
+            File = { icon = "󰈙", hl = "Tag" },
+            Namespace = { icon = "󰌗", hl = "Include" },
+            Package = { icon = "󰏖", hl = "Label" },
+            Class = { icon = "󰌗", hl = "Include" },
+            Property = { icon = "󰆧", hl = "@property" },
+            Enum = { icon = "󰒻", hl = "@number" },
+            Function = { icon = "󰊕", hl = "Function" },
+            String = { icon = "󰀬", hl = "String" },
+            Number = { icon = "󰎠", hl = "Number" },
+            Array = { icon = "󰅪", hl = "Type" },
+            Object = { icon = "󰅩", hl = "Type" },
+            Key = { icon = "󰌋", hl = "" },
+            Struct = { icon = "󰌗", hl = "Type" },
+            Operator = { icon = "󰆕", hl = "Operator" },
+            TypeParameter = { icon = "󰊄", hl = "Type" },
+            StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+        }
+    },
+})
+
+require('mini.pairs').setup()
+require('mini.surround').setup()
+
+require('mini.bufremove').setup()
+require("mini.diff").setup({
+    view = {
+        style = "sign",
+        signs = { add = "▎", change = "▎", delete = "▎" },
+    },
+})
+
+require('mini.cursorword').setup()
+require('mini.indentscope').setup()
+require('mini.trailspace').setup()
+
+require('which-key').setup({ preset = "helix" })
 require("which-key").add({
     { "<leader>",  group = "leader" },
 
@@ -118,11 +209,14 @@ require("which-key").add({
     { "<leader>g", group = "git" },
     { "<leader>q", group = "quit" },
 
-    { "<leader>w", proxy = "<c-w>", group = "windows" },
+    { "<leader>b", group = "buffers" },
+    { "<leader>w", group = "windows" },
+
 
     { "[",         group = "prev" },
     { "]",         group = "next" },
     { "g",         group = "goto" },
     { "z",         group = "fold" },
+    { "s",         group = "surround" },
 })
 
